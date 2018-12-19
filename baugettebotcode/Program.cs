7e43968ec3e165c9fbef60acfe684fa08f25e107
zzWeeZz69 <zzWeeZznice@gmail.com>
2018-12-19 18:49:42 +0100

    Add project files.

---
+++ baugettebotcode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace baugettebotcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
